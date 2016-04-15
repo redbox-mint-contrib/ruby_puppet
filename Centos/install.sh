840457d90f326cb515cd4920daf1bf0d690ff1de
mattRedBox <matt@redboxresearchdata.com.au>
2016-04-15 11:16:54 +1000

    updated signature for rvm.

--- Centos/install.sh
+++ Centos/install.sh
@@ -35,7 +35,7 @@ reset() {
 install_rvm() {
  yum -y install which tar
  log_function $FUNCNAME
- gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+ gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
  curl -L get.rvm.io | bash -s stable --auto-dotfiles
 }
 
