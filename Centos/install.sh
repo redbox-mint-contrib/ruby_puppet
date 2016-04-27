6c26a676242ebd304923d8bc3c5a26e79230f9ae
mattRedBox <matt@redboxresearchdata.com.au>
2016-04-28 09:41:19 +1000

    removed sudo for those test environments without tty.

--- Centos/install.sh
+++ Centos/install.sh
@@ -36,7 +36,7 @@ install_rvm() {
  yum -y install which tar
  log_function $FUNCNAME
  #gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
- curl -sSL https://rvm.io/mpapis.asc | sudo gpg2 --import -
+ curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
  curl -L get.rvm.io | bash -s stable --auto-dotfiles
 }
 
